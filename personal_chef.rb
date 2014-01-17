05963c25d5eaaf841a68efc00bcf26e3d47b980c
jhaburne <jhaburne@comcast.net>
2014-01-16 19:59:45 -0700

    Update with Butler and new PersonalChef skillz

--- personal_chef.rb
+++ personal_chef.rb
@@ -1,12 +1,21 @@
 class PersonalChef
   
-  def make_toast
-    puts "Making your toast!"
+  def make_toast(color)
+    puts "Making your #{ color } toast!"
   end
 
-  def make_milkshake
-    puts "Oh now you're thirsty?"
+  def make_milkshake(flavor)
+    puts "Oh now you're thirsty for a #{ flavor } milkshake?"
   end
 
 end
+
+class Butler
+
+  def open_door(type)
+    puts "Somebody's at the #{ type } door."
+  end
+
+end
+
 
